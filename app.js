52e7295f3b425b9ebd3f28e69c704eb09f781b9e
Abdulazeez_A <97477565+shy-programmer@users.noreply.github.com>
2025-02-05 14:33:13 +0100

    adding an input homepage

--- app.js
+++ app.js
@@ -3,27 +3,43 @@ const express = require('express');
 const cors = require('cors');
 const app = express()
 const axios = require('axios');
+const {urlencoded} = require('body-parser');
 const port = 3000
 
 app.use(cors());
+app.use(urlencoded({extended: true}));
 
 app.get('/', (req, res) => {
-    res.json({message: 'Welcome to my HNG task 1 submission. To get fun facts and details about any number add /api/classify-number/ to the url and add the number you want to classify as a query parameter. E.g. /api/classify-number/?number=371'})
+    res.sendFile(__dirname + '/index.html');
 })
 
 app.get('/api/classify-number/', async (req, res) => {
     numInput = req.query.number;
     let result;
     if (!isNaN(numInput) && numInput % 1 == 0) {
-        const APIUrl = `http://numbersapi.com/${numInput}/math`
+    const APIUrl = `http://numbersapi.com/${numInput}/math`
     const fact = await axios.get(APIUrl);
     const fun_fact = fact.data;
     result = props(numInput);
     result.fun_fact = fun_fact;}
     else {
         result = props(numInput);
-        console.log('here')
-    }
+        }
+    
+    res.status(200).json(result);
+})
+.post('/api/classify-number/', async (req, res) => {
+    numInput = req.body.entry;
+    let result;
+    if (!isNaN(numInput) && numInput % 1 == 0) {
+    const APIUrl = `http://numbersapi.com/${numInput}/math`
+    const fact = await axios.get(APIUrl);
+    const fun_fact = fact.data;
+    result = props(numInput);
+    result.fun_fact = fun_fact;}
+    else {
+        result = props(numInput);
+         }
     
     res.status(200).json(result);
 })
